e143536442f76f74342f190270adfdb5cef60cbf
wsavage <wsavage@hubspot.com>
2023-06-30 15:06:19 -0400

    Added hand code

--- mao_model/hand.py
+++ mao_model/hand.py
@@ -1,3 +1,17 @@
+from typing import List
+
+from mao_model.card import Card
+
+
 class Hand:
-    def __init__(self):
-        pass
+    def __init__(self, cards: List[Card]):
+        self.cards = cards
+
+    def play(self, idx: int):
+        return self.cards.pop(idx)
+
+    def peek(self, idx: int):
+        return self.cards[idx]
+
+    def add(self, card: Card):
+        self.cards.append(card)
--- mao_model/mao_game.py
+++ mao_model/mao_game.py
@@ -2,9 +2,6 @@ class MaoGame:
     def __init__(self):
         pass
 
-    def main(self):
-        pass
-
 
 if __name__ == '__main__':
     game = MaoGame()
