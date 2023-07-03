3bc274eb593ff36b1673e10ea5be4d4d149fa60f
wsavage <wsavage@hubspot.com>
2023-07-02 21:43:40 -0400

    Fixing serializing

--- mao_model/rule.py
+++ mao_model/rule.py
@@ -1,27 +1,42 @@
+import json
+
 from mao_model.action import Action
 from mao_model.mao_event import MaoEvent
 from mao_model.mao_game import MaoGame
 
 
 class Rule:
-    def __init__(self, eventSchema: MaoEvent, postive_action: Action, negative_action: Action):
+    def __init__(self, name: str, eventSchema: MaoEvent, positive_action: Action, negative_action: Action):
+        self.name = name
         self.eventSchema: MaoEvent = eventSchema
-        self.postive_action: Action = postive_action
+        self.positive_action: Action = positive_action
         self.negative_action: Action = negative_action
 
     def handle_event(self, event: MaoEvent, game: MaoGame):
         if self.eventSchema.match(event):
-            self.postive_action.run(event, game)
+            self.positive_action.run(event, game)
         else:
             self.negative_action.run(event, game)
 
     def to_dict(self):
-        return {"eventSchema": self.eventSchema.to_dict(),
-                "postive_action": self.postive_action.to_dict(),
+        return {"name": self.name,
+                "eventSchema": self.eventSchema.to_dict(),
+                "positive_action": self.positive_action.to_dict(),
                 "negative_action": self.negative_action.to_dict()}
 
     @classmethod
     def from_dict(cls, json_dict):
-        return cls(eventSchema=MaoEvent.from_dict(json_dict["eventSchema"]),
-                   postive_action=Action.from_dict(json_dict["postive_action"]),
+        return cls(name=json_dict["name"], eventSchema=MaoEvent.from_dict(json_dict["eventSchema"]),
+                   positive_action=Action.from_dict(json_dict["positive_action"]),
                    negative_action=Action.from_dict(json_dict["negative_action"]))
+
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict())
+
+    @classmethod
+    def from_json(cls, json_str):
+        json_ = json.loads(json_str)
+        if isinstance(json_, dict):
+            return cls.from_dict(json_)
+        elif isinstance(json_, list):
+            return [cls.from_dict(dict_) for dict_ in json_]
--- mao_ptui/__main__.py
+++ mao_ptui/__main__.py
@@ -1,13 +1,16 @@
 from argparse import ArgumentParser
 
 from mao_model.deck import Deck
+from mao_model.mao_event import MaoEvent
 from mao_model.mao_game import MaoGame
+from mao_model.rule import Rule
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="Running mao on the command line")
     parser.add_argument("--players", nargs="+", required=True, dest="players")
+    parser.add_argument("--rules", nargs="*", dest="rules")
     args = parser.parse_args()
-    game = MaoGame({}, {}, [])
+    game = MaoGame({}, {}, args.rules + [Rule("test", MaoEvent(player=".*"))])
     for player in args.players:
         game.addPlayer(player)
     game.addDeck(deck_name="Deck", deck=Deck.get_shuffled_deck())
