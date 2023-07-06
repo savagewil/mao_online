01ec25051f136aad982a57224ec1db86a52150c0
wsavage <wsavage@hubspot.com>
2023-07-06 00:28:20 -0400

    Moved rules to json

--- mao_ptui/__main__.py
+++ mao_ptui/__main__.py
@@ -1,121 +1,27 @@
+import json
 from argparse import ArgumentParser
+from typing import List
 
 from mao_model.evaluation.eval import Eval
 from mao_model.mao_game import MaoGame
 from mao_ptui.rendering_utils import render_hand, render_deck
 
+
+def load_rules(rule_files: List[str]):
+    rules = {}
+    for rule_file_path in rule_files:
+        rules = dict(**rules, **json.load(open(rule_file_path, 'r')))
+    return {rule_name: Eval.deserialize(rule) for rule_name, rule in rules.items()}
+
+
 if __name__ == '__main__':
     parser = ArgumentParser(description="Running mao on the command line")
     parser.add_argument("--players", nargs="+", required=True, dest="players")
-    parser.add_argument("--rules", nargs="*", dest="rules", default={})
+    parser.add_argument("--rules", nargs="*", dest="rules", default=[])
     parser.add_argument("--verbose", dest="verbose", action="store_true")
     args = parser.parse_args()
-    rules = {
-        "startup": Eval.deserialize(["if", ["equals", "start", "event.type"],
-                                     ["run_all",
-                                      ["set", "game.turn", 0],
-                                      ["set", "game.player_order", []],
-                                      ["add_deck", "Deck"],
-                                      ["add_deck", "play", "False", "True"],
-                                      ["deal_cards", "Deck", "play", 1],
-                                      ["set", "game.drew", False]
-                                      ]]),
 
-        "player_reg": Eval.deserialize(["if", ["equals", "player", "event.type"],
-                                        ["run_all",
-                                         ["set", "game.player_order",
-                                          ["append", "game.player_order", "event.player"]],
-                                         ["draw_cards", "event.player", "Deck", 7]]]),
-
-        "PLaying": Eval.deserialize(
-            ["if",
-             ["all",
-              ["equals",
-               "chat", "event.type"],
-              [["equals", "event.player",
-                ["get_list", "game.player_order", "game.turn"]]],
-              ["any",
-               ["match", "\\s*play\\s*\d+\\s*", "event.message"],
-               ["match", "\\s*\d+\\s*", "event.message"]]],
-             ["if_else", ["any",
-                          ["equals",
-                           ["get_dict", ["get_list", "event.player_dict.hand", ["find", "\d+", "event.message"]],
-                            "suit"], "game.decks.play.top_card.suit"],
-                          ["equals",
-                           ["get_dict", ["get_list", "event.player_dict.hand", ["find", "\d+", "event.message"]],
-                            "rank"], "game.decks.play.top_card.rank"]],
-              ["run_all",
-               ["play_card", "event.player", "play",
-                ["find", "\d+", "event.message"]],
-               ["set", "game.turn",
-                ["mod", ["inc", "game.turn"], ["len", "game.player_order"]]]],
-              ["run_all",
-               ["send_chat", "computer", "That card is not legal"]]]]),
-
-        "drawing": Eval.deserialize(
-            ["if",
-             ["all",
-              ["equals",
-               "chat", "event.type"],
-              [["equals", "event.player",
-                ["get_list", "game.player_order", "game.turn"]]],
-              ["any",
-               ["match", "\\s*draw\\s*", "event.message"],
-               ["match", "\\s*d\\s*", "event.message"]]],
-             ["run_all",
-              ["draw_cards", "event.player", "Deck", 1],
-              ["set", "game.drew", True]]]),
-
-        "passing": Eval.deserialize(
-            ["if",
-             ["all",
-              ["equals",
-               "chat", "event.type"],
-              [["equals", "event.player",
-                ["get_list", "game.player_order", "game.turn"]]],
-              ["any",
-               ["match", "\\s*pass\\s*", "event.message"],
-               ["match", "\\s*p\\s*", "event.message"]]],
-             ["if_else",
-              "game.drew",
-              ["run_all",
-               ["set", "game.turn",
-                ["mod", ["inc", "game.turn"], ["len", "game.player_order"]]]],
-              ["send_chat", "computer", "You must draw before passing"]]]),
-
-        "drawing_reset": Eval.deserialize(
-            ["if",
-             ["all",
-              ["equals",
-               "property_update", "event.type"],
-              ["equals", "turn", "event.property"]],
-             ["run_all",
-              ["set", "game.drew", False]
-              ]]),
-
-        "winning": Eval.deserialize(
-            ["if",
-             ["all",
-              ["equals",
-               "play", "event.type"],
-              ["not", "event.player_dict.hand"]],
-             ["run_all",
-              ["send_chat", "computer", ["format", "%s has won the game", "event.player"]],
-              ["draw_cards", "event.player", "Deck", 7]]]),
-
-        "reshuffling": Eval.deserialize(
-            ["if",
-             ["all",
-              ["equals",
-               "draw", "event.type"],
-              ["equals", "Deck", "event.deck"],
-              ["equals", 0, "event.deck_dict.card_count"]],
-             ["run_all",
-              ["send_chat", "computer", "Deck Empty"],
-              ["move_and_shuffle", "play", "Deck", ["sub", "game.decks.play.card_count", 1]]]]),
-
-    }
-    game = MaoGame({}, {}, dict(**args.rules, **rules), verbose=args.verbose)
+    game = MaoGame({}, {}, load_rules(args.rules), verbose=args.verbose)
     game.start_game()
     for player in args.players:
         game.addPlayer(player)
